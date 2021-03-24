34cb5161c8f4a0ef1791e6091d53a905ffe48d32
Ana <anayfernandez95@gmail.com>
2021-03-24 12:28:58 -0500

    Primeras funcionalidades

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-module.exports = () => {
-  // ...
+const marked = require("marked");
+const fs = require("fs");
+const FileHound = require("filehound");
+const fetch = require("node-fetch");
+const path = require("path");
+const { resolve } = require("path");
+const { statsAndValidateLinks } = require("./user");
+
+//Constante para obtener el directorio actual
+const directorio = process.cwd();
+//constante con un objeto vacio, para llevarlo a los test
+const mdLinks = {};
+//constante de prueba
+const test = "./test/dir-tests";
+//constante de prueba para la lectura de archivos
+const readFile = "README.md";
+//constante de prueba para la lectura de archivcos y obtener los links
+const fileHtml = "hola.html";
+//función que verifica la ruta ingresada
+const existRoute = (route) =>
+  fs.existsSync(route) ? route : "La ruta ingresada no existe";
+//para convertir ruta de relativa a absoluta
+const convertPath = (pathRoute) => {
+  try {
+    return path.resolve(pathRoute);
+  } catch (err) {
+    return "error";
+  }
 };
+console.log(convertPath(directorio));
+//para validar si archivo o directorio existen
+const existFile = (route) =>
+  fs.existsSync(route) ? route : "El archivo o directorio no existen";
+console.log(existFile(test));
+
+//para validar si es un archivo o un directorio
+const isDirOrFile = (test) => {
+  const isDir = fs.statSync(test);
+  return isDir.isFile()
+    ? "La ruta ingresada es un archivo" + test
+    : "La ruta ingresada es un directorio:" + test;
+};
+console.log(isDirOrFile(test));
+//función para recorrer un directorio y encontrar archivo .md
+
+mdLinks.convertPath = convertPath;
+mdLinks.existFile = existFile;
+mdLinks.existRoute = existRoute;
+mdLinks.isDirOrFile = isDirOrFile;
+module.exports = mdLinks;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,48 @@
-const mdLinks = require('../');
+const mdLinks = require("../index.js");
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe("función mdLink  para convertir ruta relativa a absoluta", () => {
+  it("is a functión", () => {
+    expect(typeof mdLinks.convertPath).toBe("function");
   });
 
+  it("error al cambiar de ruta", () => {
+    expect(mdLinks.convertPath()).toBe("error");
+  });
+});
+describe("funcion existFile para validar que archivo o directorio existan ", () => {
+  test(`Se espera que retorne ./test/dir-tests`, () => {
+    expect(mdLinks.existFile("./test/dir-tests")).toBe("./test/dir-tests");
+  });
+  test(`Se espera que retorne "El archivo o directorio no existen"`, () => {
+    expect(mdLinks.existFile("./test/dir-tests/pizza")).toBe(
+      "El archivo o directorio no existen"
+    );
+  });
+});
+describe("función existRoute para validar la existencia de la ruta ", () => {
+  test(`Se espera que retorne ./test/dir-tests`, () => {
+    expect(mdLinks.existRoute("./test/dir-tests")).toBe("./test/dir-tests");
+  });
+  test(`Se espera que retorne "La ruta ingresada no existe"`, () => {
+    expect(mdLinks.existRoute("./test/dir-tests/pizza")).toBe(
+      "La ruta ingresada no existe"
+    );
+  });
+});
+describe("Función isDirOrFile para verificar que sea un archivo o un directorio ", () => {
+  test(`Se espera que retorne ./test/dir-tests`, () => {
+    expect(mdLinks.isDirOrFile("./test/dir-tests")).toBe(
+      "La ruta ingresada es un directorio:./test/dir-tests"
+    );
+  });
+  test(`Se espera que retorne "La ruta ingresada es un archivo"`, () => {
+    expect(mdLinks.isDirOrFile("./test/dir-tests/chao.md")).toBe(
+      "La ruta ingresada es un archivo"
+    );
+  });
+  test(`Se espera que retorne "La ruta ingresada es un directorio"`, () => {
+    expect(mdLinks.isDirOrFile("./test/dir-tests")).toBe(
+      "La ruta ingresada es un directorio"
+    );
+  });
 });
