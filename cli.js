cbc4f603e5a993af2dddfdad3103b6508b895ec3
Ana <anayfernandez95@gmail.com>
2021-04-02 15:09:29 -0500

    cli funcionalidad

--- cli.js
+++ cli.js
@@ -1,17 +1,48 @@
-const mdLink = require("./md-links.js");
+const mdLinks = require("./index.js");
+const utils = require("./md-links.js");
+const { Command, option } = require("commander");
 
-const test = __dirname + "/test";
-//constante de prueba para la lectura de archivos
+const chalk = require("chalk");
 
-const validate = proccess.argv.indexOf("--validate") > 0;
-const stats = proccess.argv.indexOf("--stats") > 0;
+const program = new Command();
+program.option(
+  "-v, --validate",
+  "to validate the links inside of a markdown file",
+  false
+);
+program.option(
+  "-s, --stats",
+  "to show basic stats about the links (total of links and unique ones)",
+  false
+);
 
-mdLinks(test, { validate: validate })
-  .then((res) => {
-    res.forEach((link) => {
-      console.log(link.file, link.href, link.text);
+program.parse(process.argv);
+let options = {
+  validate: program._optionValues.validate,
+  stats: program._optionValues.stats,
+};
+
+mdLinks(process.argv[2], {
+  validate: options.validate,
+}).then((links) => {
+  if (options.stats) {
+    let stats = utils.stats(links);
+    console.log("Total: ", links.length);
+    console.log("Unique: ", stats.unique);
+    if (options.validate) {
+      console.log("Broken: ", stats.broken);
+    }
+  } else {
+    links.forEach((link) => {
+      console.log(
+        `${link.file} ${chalk.blue(link.href)} ${
+          options.validate
+            ? link.status === "OK"
+              ? chalk.green(link.status) + " " + link.statusCode
+              : chalk.red(link.status) + " " + link.statusCode
+            : ""
+        } ${link.text}`
+      );
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  }
+});
